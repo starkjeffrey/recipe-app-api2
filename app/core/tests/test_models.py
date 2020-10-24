eba1d2554f0b24aa75eecb4d475a1f2f6d4a755a
starkjeffrey <61903972+starkjeffrey@users.noreply.github.com>
2020-10-24 18:04:28 +0700

    Add user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating that a new user with email is successful"""
+        email = 'poopshitfuck@jeff.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test that the email for the new user is normalized"""
+        email = 'test@JEFF.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email to raise error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'pass123')
+
+    def test_create_new_superuser(self):
+        """Test that we can create a proper superuser"""
+        user = get_user_model().objects.create_superuser(
+            'poop@jeff.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
